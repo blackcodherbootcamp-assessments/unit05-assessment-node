cdc7764422e6605e02ee5e8a18f68d6261f7cb16
Gary McPherson <genyus@gmail.com>
2023-02-08 21:44:38 +0000

    feat: fixes and implements autograding tests
    * style: applies Prettier formatting
    
    * style: updates formatting for test-todos.json
    
    * feat: implements initial index.html response
    
    * style: formats apitest.js with Prettier
    
    * test: refactors app to enable cleaner testing
    
    Extracts listen statement to allow Mocha tests to exit cleanly
    Wraps import of todos.json to ensure consistent test results
    
    * test: generates test data more cleanly
    
    Sets dates for test todos relative to current date
    Removes auto-generated test file from source control
    Executes file generation before, rather than after test runs
    
    * test: makes tests more consistent
    
    Makes all describe() callbacks asynchronous
    Makes response content type checking stricter (e.g. text/html vs html)
    
    * test: improves file loading
    
    Replaces hard-coded file path with environment variable
    
    * test: improves overdue endpoint tests
    
    Adds checks for response type, length and completion state
    
    * test: improves overdue endpoint tests
    
    Refactors for readability
    
    * test: improves completed endpoint tests
    
    Adds checks for response type, length and completion status
    
    * refactor: improves file path generation
    
    * chore: adds error handling middleware
    
    * chore: improves root route
    
    Hands off error
    
    * feat: implements GET /todos route
    
    * feat: implements GET /todos/overdue route
    
    * feat: implements GET /todos/completed route
    
    * feat: implements POST /todos route
    
    * feat: implements /todos/:id PATCH route
    
    * fix: sets correct Content-Type
    
    * feat: implements /todos/:id/complete route
    
    * test: checks for correct HTTP status codes
    
    Updates expected status code for invalid ids from 400 to 404
    
    * feat: implements /todos/:id/undo route
    
    * fix: updates PATCH route to save correctly
    
    * feat: implements DELETE /todos/:id route
    
    * ci: Adds autograding workflow
    
    * ci: sets Node version in Classroom workflow to v16
    
    * build: updates package.lock version
    
    * chore(deps): updates devDependencies

--- src/index.js
+++ src/index.js
@@ -1,54 +1,210 @@
-require('dotenv').config();
-const fs = require('fs');
-const express = require('express');
+require("dotenv").config();
+const fs = require("fs");
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const port = 8080;
-const bodyParser = require('body-parser');
-const { v4: uuidv4 } = require('uuid');
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
 const todoFilePath = process.env.BASE_JSON_PATH;
 
-//Read todos from todos.json into variable
-let todos = require(__dirname + todoFilePath);
+// Read todos from todos.json into variable
+const getTodos = () => require(path.join(__dirname, todoFilePath));
 
+// Save todos to disk
+const saveTodos = async (todos) => {
+  await fs.writeFile(
+    path.join(__dirname, todoFilePath),
+    JSON.stringify(todos, null, 2) + "\n",
+    (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    }
+  );
+};
+
+// Error handling Middleware function for logging the error message
+const errorLogger = (error, request, response, next) => {
+  console.log(`error: ${error.message}`);
+};
+
+// Error handling Middleware function reads the error message
+// and sends back a response in JSON format
+const errorResponder = (error, request, response, next) => {
+  response.header("Content-Type", "application/json");
+
+  const status = error.status || 400;
+  response.status(status).send(error.message);
+};
+
+// Fallback Middleware function for returning
+// 404 error for undefined paths
+const invalidPathHandler = (request, response, next) => {
+  response.status(404);
+  response.send("Invalid path");
+};
+
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.raw());
 app.use(bodyParser.json());
-
 app.use("/content", express.static(path.join(__dirname, "public")));
 
-app.get("/", (_, res) => {
-  /*
-  res.sendFile("./public/index.html", { root: __dirname });
-  */
-  res.status(501).end();
+app.get("/", (req, res) => {
+  res.sendFile("./public/index.html", { root: __dirname }, (err) => {
+    if (err) {
+      console.log(err);
+      next(err);
+    }
+  });
 });
 
-app.get('/todos', (_, res) => {
-  /*
-  res.header("Content-Type","application/json");
-  res.sendFile(todoFilePath, { root: __dirname });
-  */
-  res.status(501).end();
+app.get("/todos", (req, res) => {
+  try {
+    const todos = getTodos();
+
+    res.setHeader("Content-Type", "application/json").send(todos);
+  } catch (err) {
+    next(err);
+  }
 });
 
 //Add GET request with path '/todos/overdue'
+app.get("/todos/overdue", (req, res) => {
+  try {
+    const todos = getTodos().filter(
+      (todo) => new Date(todo.due) < new Date() && todo.completed === false
+    );
+
+    res.setHeader("Content-Type", "application/json").send(todos);
+  } catch (err) {
+    next(err);
+  }
+});
 
 //Add GET request with path '/todos/completed'
+app.get("/todos/completed", (req, res) => {
+  try {
+    const todos = getTodos().filter((todo) => todo.completed === true);
+
+    res.setHeader("Content-Type", "application/json").send(todos);
+  } catch (err) {
+    next(err);
+  }
+});
 
 //Add POST request with path '/todos'
+app.post("/todos", (req, res) => {
+  const newTodo = req.body;
+
+  if (!newTodo.name || !newTodo.due) {
+    res.status(400).send("Invalid request");
+  } else {
+    const todos = getTodos();
+
+    newTodo.id = uuidv4();
+    newTodo.completed = false;
+    newTodo.created = new Date().toISOString();
+    todos.push(newTodo);
+    saveTodos(todos)
+      .then((value) => {
+        res
+          .setHeader("Content-Type", "application/json")
+          .status(201)
+          .send(newTodo);
+      })
+      .catch((err) => next(err));
+  }
+});
 
 //Add PATCH request with path '/todos/:id
+app.patch("/todos/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    const todos = getTodos();
+    const todo = todos.find((todo) => todo.id === id);
+
+    if (todo) {
+      todo.name = req.body.name ?? todo.name;
+      todo.due = req.body.due ?? todo.due;
+      todo.completed = req.body.completed ?? todo.completed;
+      saveTodos(todos)
+        .then((value) => {
+          res.setHeader("Content-Type", "application/json").send(todo);
+        })
+        .catch((err) => next(err));
+    } else {
+      res.status(404).send("Todo not found");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
 //Add POST request with path '/todos/:id/complete
+app.post("/todos/:id/complete", (req, res) => {
+  try {
+    const id = req.params.id;
+    const todos = getTodos();
+    const todo = todos.find((todo) => todo.id === id);
+
+    if (todo) {
+      todo.completed = true;
+      res.setHeader("Content-Type", "application/json").send(todo);
+    } else {
+      res.status(404).send("Todo not found");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
 //Add POST request with path '/todos/:id/undo
+app.post("/todos/:id/undo", (req, res) => {
+  try {
+    const id = req.params.id;
+    const todos = getTodos();
+    const todo = todos.find((todo) => todo.id === id);
+
+    if (todo) {
+      todo.completed = false;
+      res.setHeader("Content-Type", "application/json").send(todo);
+    } else {
+      res.status(404).send("Todo not found");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
 
 //Add DELETE request with path '/todos/:id
+app.delete("/todos/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    const todos = getTodos();
+    const index = todos.findIndex((todo) => todo.id === id);
 
-app.listen(port, function () {
-    console.log(`Node server is running... http://localhost:${port}`);
+    if (index >= 0) {
+      todos.splice(index, 1)
+      saveTodos(todos)
+        .then((value) => {
+          res.send();
+        })
+        .catch((err) => next(err));
+    } else {
+      res.status(404).send("Todo not found");
+    }
+  } catch (err) {
+    next(err);
+  }
 });
+
+// Attach error handling middleware after all regular middleware
+app.use(errorLogger);
+app.use(errorResponder);
+app.use(invalidPathHandler);
 
 module.exports = app;
