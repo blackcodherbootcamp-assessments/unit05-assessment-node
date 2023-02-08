aaa68593d4e744b6e66a606a166f65787397a82c
Genyus <genyus@gmail.com>
2023-02-08 04:24:46 +0000

    chore: adds error handling middleware

--- src/index.js
+++ src/index.js
@@ -11,12 +11,32 @@ const todoFilePath = process.env.BASE_JSON_PATH;
 // Read todos from todos.json into variable
 const getTodos = () => require(path.join(__dirname, todoFilePath));
 
+// Error handling Middleware function for logging the error message
+const errorLogger = (error, request, response, next) => {
+  console.log(`error: ${error.message}`);
+};
+
+// Error handling Middleware function reads the error message
+// and sends back a response in JSON format
+const errorResponder = (error, request, response, next) => {
+  response.header("Content-Type", "application/json");
+
+  const status = error.status || 400;
+  response.status(status).send(error.message);
+};
+
+// Fallback Middleware function for returning
+// 404 error for undefined paths
+const invalidPathHandler = (request, response, next) => {
+  response.status(404);
+  response.send("Invalid path");
+};
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.raw());
 app.use(bodyParser.json());
-
 app.use("/content", express.static(path.join(__dirname, "public")));
 
 app.get("/", (_, res) => {
@@ -48,5 +68,10 @@ app.get("/todos", (_, res) => {
 //Add POST request with path '/todos/:id/undo
 
 //Add DELETE request with path '/todos/:id
+
+// Attach error handling middleware after all regular middleware
+app.use(errorLogger);
+app.use(errorResponder);
+app.use(invalidPathHandler);
 
 module.exports = app;
