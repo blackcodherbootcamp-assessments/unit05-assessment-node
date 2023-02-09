f2a6f562924fc24e36f8d546d0146bc3c9f3eac4
Genyus <genyus@gmail.com>
2023-02-09 18:15:52 +0000

    fix: fixes static middleware root path

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ const invalidPathHandler = (request, response, next) => {
   response.send("Invalid path");
 };
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.raw());
